4d4fde2e48a6d84cc87e7c9e358fdfd7bc04a543
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-07-27 16:09:34 +0100

    Updates

---
+++ precache-manifest.d587f16ec6a9a7fd7e948ffba7b40cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ed364f51ecda00e7dcb82ab672666d",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "f8a0fbe92476d090ef67",
+    "url": "/reactcalculator/static/css/main.68d43b25.chunk.css"
+  },
+  {
+    "revision": "60ff9b6b9c009ded58ac",
+    "url": "/reactcalculator/static/js/2.e5c4be3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcalculator/static/js/2.e5c4be3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a0fbe92476d090ef67",
+    "url": "/reactcalculator/static/js/main.1a6c426a.chunk.js"
+  },
+  {
+    "revision": "89dafa6b138e96b1e729",
+    "url": "/reactcalculator/static/js/runtime-main.4261398c.js"
+  }
+]);
